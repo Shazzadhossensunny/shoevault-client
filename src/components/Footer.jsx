889e65cdb92daa10e8b3f2ae1098d6f2ed3bf461
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-08-17 18:19:55 +0600

    footer add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer footer-center px-10 py-12 lg:py-24 bg-[#1c1c1d]">
+    <aside className="space-y-6">
+    <Link to="/" className="text-2xl lg:text-4xl font-semibold uppercase text-white">
+            ShoeVault
+          </Link>
+      <p className="text-white">
+        Copyright © 2024 <span className="text-orange-400">Sunny</span>. - All right reserved
+      </p>
+      <div  className="text-white">
+        <h2 className="text-lg font-semibold mb-3">General Inquiries</h2>
+        <p>
+
+        <a href="mailto:contact@info.com" className="btn-link text-white">contact@info.com</a>
+        </p>
+        <p>
+
+        <a href="tel:+985648274557" className="btn-link text-white">+985648274557</a>
+        </p>
+      </div>
+      <div className="flex text-2xl gap-5 cursor-pointer text-white">
+        <FaFacebookF></FaFacebookF>
+        <FaTwitter></FaTwitter>
+        <FaLinkedinIn></FaLinkedinIn>
+      </div>
+    </aside>
+  </footer>
   )
 }
